2738ab21b3cfdd012f6c86a21803378da7d7f504
Mat Sletten <matsletten@reception-desk.bbbb.net>
2015-01-28 17:07:02 -0800

    first commit

--- Playing Music/ViewController.swift
+++ Playing Music/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBAction func playButton(sender: AnyObject) {
+        songPlayer.play()
+    }
+    @IBOutlet weak var volumeStyle: UISlider!
+    @IBAction func volumeSlider(sender: AnyObject) {
+        songPlayer.volume = volumeStyle.value
+    }
+    @IBAction func pauseButton(sender: AnyObject) {
+        songPlayer.pause()
+    }
+    @IBAction func stopButton(sender: AnyObject) {
+        songPlayer.stop()
+        songPlayer.currentTime = 0
+    }
+    
+    var songPlayer:AVAudioPlayer = AVAudioPlayer()
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //This is to tell the app where to find the media file. NSBundle is the entire bundle that makes up the app.
+        var fileLocation = NSString(string:NSBundle.mainBundle().pathForResource("01 Sunrise (Come My Way)", ofType: "mp3"))
+        var error: NSError? = nil
+        songPlayer = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation), error: &error)
+        //songPlayer.play()
     }
 
     override func didReceiveMemoryWarning() {
